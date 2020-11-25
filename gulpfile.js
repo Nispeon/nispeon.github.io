97535831fea9394ad704e9cdc55a55835c3d16ee
Julien <Nisouc@outlook.com>
2020-11-25 20:47:15 +0100

    added sass variables

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 const { stc, dest, src, symlink, parallel, watch } = require('gulp');
-const del = require('del');
 const gulpSass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 
@@ -23,46 +22,15 @@ function sassComp(){
 
 //watch scss
 function watcher(done){
-    watch('./sass/*.scss', sassComp)
+    watch('./sass/', sassComp)
     browserSync.reload();
     done();
 }
 
-//src + dest
-function srcExemple(){
-    return src('./index.html')
-    .pipe(dest('dossier1'))
-}
-
-//symlink
-function linkExemple(){
-    return src('./index.html').pipe(symlink('link'))
-}
-
-//delete
-function clean(){
-    return del('dossier1')
-}
-
-//parallel 1
-function css(log){
-    console.log('Tâche 1, exemple de css')
-    log();
-}
-
-//parallel 2
-function sass(log){
-    console.log('Tâche 2, exemple de compilation')
-    log();
-}
 
 // Exports
 module.exports = {
-    srcExemple,
-    linkExemple,
-    clean,
     sassComp,
     watcher,
     browser: parallel(browser, watcher),
-    build: parallel(css, sass)
 }
