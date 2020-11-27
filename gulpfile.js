8ee25d92a4ffa3bf5e9974a8cf912921db45e15a
Julien <Nisouc@outlook.com>
2020-11-27 22:36:55 +0100

    :iphone started working on responsive

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const { stc, dest, src, symlink, parallel, watch } = require('gulp');
 const gulpSass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
+// const cleanCSS = require('gulp-clean-css');
 
 //browser
 function browserS(){
@@ -16,8 +17,10 @@ function browserS(){
 function sassComp(){
     return src('./sass/style.scss')
     .pipe(gulpSass())
+    // .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(dest('./css/'))
     .pipe(browserSync.stream());
+    
 }
 
 //watch scss
